cb56a4a3e54ebff4fa75f9d13d3a9dd9bf2fa238
AlanDAmario <vega93@live.it>
2024-12-02 18:27:19 +0100

    add method filter product and general fix

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { max, min } from 'rxjs';
 
+//Definiamo l interfaccia per la tipizzazione dei prodotti
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  styleUrl: './shop.component.css',
 })
 export class ShopComponent {
+  // Lista di prodotti che rispetta l'interfaccia Product
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'Laptop',
+      description: 'Un laptop potente e leggero.',
+      price: 1200,
+    },
+    {
+      id: 2,
+      name: 'Smartphone',
+      description: 'Un telefono con fotocamera avanzata.',
+      price: 800,
+    },
+    {
+      id: 3,
+      name: 'Cuffie',
+      description: 'Cuffie con cancellazione del rumore.',
+      price: 200,
+    },
+  ];
 
+  
+  //filtrare i prodotti in base al prezzo
+  filterByPrice(min: number, max: number): Product[] {
+    return this.products.filter(
+      (product) => product.price >= min && product.price <= max
+    );
+  }
 }
