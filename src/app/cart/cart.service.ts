90e95192157276d234036bc9b12e8e60eb293bc1
AlanDAmario <vega93@live.it>
2024-12-06 12:07:31 +0100

    add behaviorSubject for cart

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 //product importato da models
 import { Product } from '../models/product';
+//serve per tener traccia dello stato corrente del carrello
 import { BehaviorSubject } from 'rxjs';
 
 //interfaccia per il carrello
@@ -8,10 +9,16 @@ export interface CartProduct extends Product{
   quantity: number;
 }
 
+//questo dice ad Angular che il servizio CartService sarà disponibile globalmente nell'app, senza bisogno di dichiararlo esplicitamente in ogni modulo
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+//memorizziamo i prodotti nel carrelo partedno da un array vuoto
+  private cart: CartProduct[] = [];
+  //creiamo una nuova istanza di behaviorsubject, partenndo da un array vuoto inizializzato con this.cart
+  private cartSubject = new BehaviorSubject<CartProduct[]>(this.cart);
   constructor() { }
+  
 }
+
