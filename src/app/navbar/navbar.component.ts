acab174750a5d4946f2f68d7fc2c0153675d24af
AlanDAmario <vega93@live.it>
2024-12-23 10:09:18 +0100

    add early navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isHovered: boolean = false;
 
+  onHoverHpage() {
+    this.isHovered = true;
+  }
+
+  onHoverOutHpage() {
+    this.isHovered = false;
+  }
+  onHoverCart() {
+    this.isHovered = true;
+  }
+
+  onHoverOutCart() {
+    this.isHovered = false;
+  }
 }
