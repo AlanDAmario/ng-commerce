3c53ec678d73c4426da410df946e7be9b2fd6749
AlanDAmario <vega93@live.it>
2025-01-02 19:08:14 +0100

    add paginate method of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,4 +21,20 @@ export class ProductService {
   getProducts(): Observable<Product[]>{
     return this.http.get<Product[]>(this.apiUrl);
   }
+  //impaginazione per i prodotti
+  paginateProducts(limit: number, page: number): Observable<Product[]> {
+    // aggiungiamo l offset (il numero di prodotti da saltare per mostrare i risultati della pagina corrente ) in base alla pagina corrente
+    //per la prima pagina (page = 1), l'offset è 0, quindi mostrerà i primi limit prodotti.
+    //per la seconda pagina (page = 2), l'offset è limit, quindi mostrerà i prodotti dal (limit + 1) al (limit * 2).
+    //per la terza pagina, l'offset è (2 * limit), e così via.
+    const offset = (page - 1) * limit;
+    //costruiamo dinamicamente l URL con i parametri li it e offset
+    //?limit=${limit}: Specifica il numero massimo di prodotti da restituire.
+    //&offset=${offset}: Specifica da quale prodotto partire.
+    const pageUrl = `${this.apiUrl}?limit=${limit}&offset=${offset}`;
+    //attraverso il metodo get di httpclient facciamo una richiesta http get dell url paginato ovvero pageurl
+    //specifichiamo <Product[]> per indicare l array di oggetti di tipo product
+    //restituendo l observable che metterà i dati dei prodotti una volta che la richiesta sraà completata 
+    return this.http.get<Product[]>(pageUrl);
+  }
 }
