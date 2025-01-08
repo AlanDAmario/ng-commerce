d3c58decfa1b55992057471372a0b364b3827a4a
AlanDAmario <vega93@live.it>
2025-01-08 12:40:04 +0100

    add detail product template and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './services/cart/cart.component';
+import { ProductComponent } from './shop/product.component';
+const routes: Routes = [
+  { path: 'product/:id', component: ProductDetailComponent },
+  {path: 'products', component:ProductComponent}
+  // Rotte aggiuntive (esempio: home page)
+  // { path: '', redirectTo: '/home', pathMatch: 'full' }, // Reindirizza alla home page
+  // { path: '**', redirectTo: '/home' } // Gestione di rotte non valide
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CartService } from '../services/cart/cart.service';
+import { Product, CartProduct } from '../models/product';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css',
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
   //memorizziamo l id del prodotto
   productId: number | null = null;
+  //memorizzaare i dettagli del prodotto
+  productDetails: any = null;
   //innittiamo nelle Activatedroute come dipendeza, consentendo di accedere ai dati di routing
   constructor(
     private route: ActivatedRoute, // per gestire i parametri dell url
-    private http: HttpClient // per chiamare l api
+    private http: HttpClient, // per chiamare l api
+    private cartService: CartService
   ) {}
   //al momento dell inizializzazione del componente recuperiamo l id dall url
   ngOnInit(): void {
@@ -22,9 +27,10 @@ export class ProductDetailComponent implements OnInit {
     // .paramMap: è una mappa che contiene tutti i parametri dell'URL
     // .get('id'): prende il valore del parametro 'id' (esempio: /product/123 → id=123)
     // Number(): converte il valore da stringa a numero (se id fosse "123", diventa 123)
+    this.productDetailsApi();
   }
   //carichhiamo i dettagli del prodotto attraverso la chimata api
-  productDetails(): void {
+  productDetailsApi(): void {
     // Definizione dell'URL base dell'API
     // Questo URL rappresenta il punto di accesso all'API per recuperare i dettagli dei prodotti.
     const apiUrl = 'https://dummyjson.com/products';
@@ -59,6 +65,28 @@ export class ProductDetailComponent implements OnInit {
       // Caso in cui l'ID del prodotto non è valido (es. è null)
       // Stampiamo un errore in console per aiutare con il debug.
       console.error('ID del prodotto non valido');
+    }
+  }
+  /**
+   * Aggiungere un prodotto al carrello.
+   * @param product Prodotto da aggiungere.
+   */
+  addToCart(product: Product): void {
+    const cartProduct: CartProduct = { ...product, quantity: 1 }; // Creiamo un oggetto per il carrello
+    this.cartService.addToCart(cartProduct); // Aggiungiamo al carrello
+  }
+  ////////////////////////////////////////////////////////////////////////
+  //stars icon
+  getStarIcon(star: number, rate: number): string {
+    const floorRate = Math.floor(rate); // Parte intera del rating
+    const decimal = rate - floorRate; // Parte decimale del rating
+
+    if (star <= floorRate) {
+      return 'fas fa-star text-warning'; // Stella piena
+    } else if (star === floorRate + 1 && decimal >= 0.5) {
+      return 'fas fa-star-half-alt text-warning'; // Mezza stella
+    } else {
+      return 'far fa-star text-muted'; // Stella vuota
     }
   }
 }
