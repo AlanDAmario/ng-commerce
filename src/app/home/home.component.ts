2fa7948f6d8e7eb88c93307611c8d80074e9fa1d
AlanDAmario <vega93@live.it>
2025-01-10 13:40:39 +0100

    add general restyling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../models/product';
+import { ProductService } from '../services/api/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
-
+  products: Product[] = []; // Array per memorizzare i prodotti
+  constructor(private router: Router,
+    private productService: ProductService) { }
+  ngOnInit(): void {
+    // Ottieni i prodotti tramite il servizio
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data; // Salva i prodotti nel componente
+    });
+  }
   // Navigate to the products page
   navigateToShop(): void {
     this.router.navigate(['/products']);
