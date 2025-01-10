e85d0584e5c24c2ed10d06025966ee906e00437f
AlanDAmario <vega93@live.it>
2025-01-10 11:32:56 +0100

    add style: improved footer layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './services/cart/cart.component';
 import { ProductComponent } from './shop/product.component';
+import { FooterComponent } from './footer/footer.component';
 const routes: Routes = [
   { path: 'product/:id', component: ProductDetailComponent },
   { path: 'products', component: ProductComponent },
-  {path: 'cart', component: CartComponent}
+  { path: 'cart', component: CartComponent },
   // Rotte aggiuntive (esempio: home page)
   // { path: '', redirectTo: '/home', pathMatch: 'full' }, // Reindirizza alla home page
   // { path: '**', redirectTo: '/home' } // Gestione di rotte non valide
